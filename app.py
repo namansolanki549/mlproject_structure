7d8bc93fd84b6376f89c51878b5d231670da19f2
Naman Solanki <namansolanki549@gmail.com>
2023-11-11 20:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 import os
 
-
 application = Flask(__name__)
 
 app = application
